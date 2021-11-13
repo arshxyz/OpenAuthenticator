1c6cbed3695f47b4f9950892ccae1527bde9cc84
Arsh Kohli <kohliarsh9@gmail.com>
2021-11-13 08:46:54 +0530

    testing ipcRenderer

--- src/renderer/components/MainScreen.tsx
+++ src/renderer/components/MainScreen.tsx
@@ -3,6 +3,13 @@ import { useEffect } from 'react';
 const MainScreen = () => {
   useEffect(() => {
     console.log('test');
+    const test = {
+      name: 'name',
+      url: 'url',
+      secert: 'secret',
+      id: 'id',
+    };
+    window.electron.ipcRenderer('add', test);
   }, []);
   return <button type="button">OpenAuthenticator</button>;
 };
